9a46643f7cbb33abf5c3f3d30ef8b7f7568c3d0d
Reginaldo Bonaldi <reginaldobonaldi22@gmail.com>
2021-02-13 17:04:54 -0300

    adicionando tema base do projeto, lodash e css dos botões

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import Logo from '../../../themes/Logo'
+import Logo from '../../../theme/Logo'
+import { Button } from '../Button';
 const MenuWrapper = styled.nav`
   font-family: 'Rubik', 'sans-serif';
   display: flex;
@@ -50,7 +51,7 @@ MenuWrapper.CentralSide = styled.div`
 MenuWrapper.RightSide = styled.div`
   padding: 0;
   margin: 1px;
-  order: 3;
+  order: 2;
 `;
 
 const links =[
@@ -77,15 +78,15 @@ export default function Menu (){
       <MenuWrapper.CentralSide>
           {links.map( link => {
               return (
-                  <li>
+                  <li key={link.url} >
                       <a href={link.url}>{link.texto}</a>
                   </li>
               )
           })}
       </MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>
-          <button>Entrar</button>
-          <button>Cadastrar</button>
+          <Button ghost variant="secondary.main">Entrar</Button>
+          <Button variant="primary.main">Cadastrar</Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
   )
