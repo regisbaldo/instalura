65ffab46f169da6e23a4d612d9fc4ccbee0deae9
Reginaldo Bonaldi <reginaldobonaldi22@gmail.com>
2021-03-20 14:36:53 -0300

    feat(slug): adicionando slug da faq e refatorando a home

--- src/components/wrappers/WebsitePage/hoc/index.js
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -1,12 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import WebsitePageWrapper from '..';
 import WebSiteGlobalProvider from '../provider';
 
-export default function webSitePageHOC(PageComponent, { pageWrapperProps }) {
+export default function websitePageHOC(
+  PageComponent,
+  { pageWrapperProps } = { pageWrapperProps: {} },
+) {
   return (props) => (
     <WebSiteGlobalProvider>
-      <WebsitePageWrapper {...pageWrapperProps}>
+      <WebsitePageWrapper
+        {...pageWrapperProps}
+        {...props.pageWrapperProps}
+      >
         <PageComponent {...props} />
       </WebsitePageWrapper>
     </WebSiteGlobalProvider>
